9c5903d4427c7a925e105c647c881c891d88b432
Andrew Guthrie <157662240+goguthrie@users.noreply.github.com>
2026-04-03 18:41:33 -0400

    Initial commit

---
+++ elpa/org-outlook-20160705.1338/org-outlook-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "org-outlook" "20160705.1338"
+  "Outlook org."
+  ()
+  :url "https://github.com/mlf176f2/org-outlook.el"
+  :commit "ec32d8d9d8ffd17e6de4de0b52fc3f5ad9b4cc0d"
+  :revdesc "ec32d8d9d8ff"
+  :keywords '("org-outlook"))
