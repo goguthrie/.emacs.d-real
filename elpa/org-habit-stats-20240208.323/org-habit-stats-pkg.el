53363bdf8e783252db08508e00cc3adef55491c0
Andrew Guthrie <157662240+goguthrie@users.noreply.github.com>
2026-05-13 13:15:28 -0400

    202605131315

---
+++ elpa/org-habit-stats-20240208.323/org-habit-stats-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "org-habit-stats" "20240208.323"
+  "Display info about habits."
+  '((emacs "25.1"))
+  :url "https://github.com/ml729/org-habit-stats/"
+  :commit "d500f3a1b269b26097dd2f4cd414c3cb7c68ca23"
+  :revdesc "d500f3a1b269"
+  :keywords '("calendar" "org-mode" "org-habit" "habits" "stats" "statistics" "charts" "graphs"))
